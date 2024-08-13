a81ee24b826d39ddca20258ab66d32fd51d5f833
williamrx777 <williamr723@gmail.com>
2024-08-13 19:56:35 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        var curso = new Curso();
+        var mentoria = new Mentoria();
+
+        curso.setTitulo("curso java");
+        curso.setDescricao("descrição curso java");
+        curso.setCargaHoraria(8);
+
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
         }
     }
