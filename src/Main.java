346ea99b5ee44a2db024f7fd225ace3fdf11fd19
williamrx777 <williamr723@gmail.com>
2024-08-13 21:51:13 -0300

    implementando curso conteudo bootcamp dev mentoria e melhorias

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -8,18 +7,37 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        var curso = new Curso();
-        var mentoria = new Mentoria();
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
 
         curso.setTitulo("curso java");
         curso.setDescricao("descrição curso java");
         curso.setCargaHoraria(8);
 
+
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
+
+/*
         System.out.println(curso);
         System.out.println(mentoria);
-        }
+        */
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(mentoria);
+        System.out.println("----------");
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudo Inscritos João" + devJoao.getConteudoInscritos());
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteudo Concluidos João" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
+
+    }
     }
--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -9,4 +9,27 @@ public abstract class Conteudo {
 
     public abstract  double calcularXp();
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Conteudo{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
 }
